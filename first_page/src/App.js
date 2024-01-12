e7c782d49477721e20eaff765316e17f3564ad3c
Eliel S <hydrahtec@gmail.com>
2024-01-11 23:12:33 -0300

    primeiro projeto react

---
+++ first_page/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Primeiro projeto</h1>
+        <p>testando comandos</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
