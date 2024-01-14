17ea95d51e25efe138969567af31f52a685012c7
Eliel S <hydrahtec@gmail.com>
2024-01-14 10:55:10 -0300

    update project hooks

---
+++ page-hooks/src/componentes/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css'
+
+export default function Footer() {
+
+    return (
+        <div>
+            <footer>
+                <span>
+                    Desencolvido por Hydrah Tecnologia
+                </span>
+            </footer>
+        </div>
+    )
+};
