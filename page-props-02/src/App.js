1a23b1c2d0c8d5c9b877452446141f8ee81e25b1
Eliel S <hydrahtec@gmail.com>
2024-01-15 23:39:25 -0300

    props v 1.0

--- page-props-02/src/App.js
+++ page-props-02/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import {useState} from 'react';
+import imagemCachorro from './assets/cachorro.jpg';
+import imagemGato from './assets/gato.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+      let imagemAnimal = "";
+      let informacaoAnimal = "";
+      let tipoAnimal = "Cachorro";
+
+      const [tipoDoComponenteCard, setTipoDoComponenteCard] = useState ("cachorro");
+
+      const alterarState = () => {
+        if (tipoDoComponenteCard === "cachorro") {
+          setTipoDoComponenteCard("gato");
+         } else {
+          setTipoDoComponenteCard("cachorro");
+        }
+      };
+  
+      if (tipoDoComponenteCard === "cachorro") {
+        imagemAnimal = imagemCachorro;
+        informacaoAnimal = "É um mamífero carnívoro da família dos canídeos.";
+        tipoAnimal = "Cachorro";
+      } else {
+        imagemAnimal = imagemGato;
+        informacaoAnimal = "É um mamífero carnívoro da família dos felídeos.";
+        tipoAnimal = "Gato";
+      }
+
+
+    return (
+      <div className="App">
+        {/* componente Topo */}
+        <header>
+          <h1>Projeto Props + useState</h1>
+          <p>Clique no botão para mudar os componentes abaixo</p>
+          <button onClick={alterarState}>Mudar animal</button>
+        </header>
+
+        {/* Compomente CardAnimal */}
+        <div id="CardAnimal">
+          <img src={imagemAnimal} alt="animal" />
+        </div>
+
+        {/* Componente CardInformacoes */}
+        <div className="CardInformacao">
+          <h3>Informação sobre o animal</h3>
+          <p>{tipoAnimal}, {informacaoAnimal}</p>
+        </div>
+      </div>
+    );
 }
-
-export default App;
