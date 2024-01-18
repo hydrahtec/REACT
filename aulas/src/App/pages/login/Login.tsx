917abed009e56ccf8e7971e0b393f8208c30c804
Eliel S <hydrahtec@gmail.com>
2024-01-18 19:37:12 -0300

    recuperação de dados de forms

--- aulas/src/App/pages/login/Login.tsx
+++ aulas/src/App/pages/login/Login.tsx
@@ -16,26 +16,34 @@
 //     );
 // };
 
+import {useState} from 'react';
+
 export const Login = () => {
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
    
-   
-    return (
-        <div>
-            <form>
-                <label>
-                    <span>Email: </span>
-                    <input />
-                </label>
+ const cliqueBotao = () => {
+    console.log(email);
+    console.log(password);
+ };
 
-                <label>
-                    <span>Senha: </span>
-                    <input type="password" />
-                </label>
+return (
+    <div>
+        <form>
+            <label>
+                <span>Email: </span>
+                <input value={email} onChange={e => setEmail(e.target.value) } />
+            </label>
 
-                <button type="button" >
-                    Entrar
-                </button>
-            </form>
-        </div>
+            <label>
+                <span>Senha: </span>
+                <input type="password" value={password} onChange={e => setPassword(e.target.value) } />
+            </label>
+
+            <button type="button" onClick={cliqueBotao} >
+                Entrar
+            </button>
+        </form>
+    </div>
     );
 };
