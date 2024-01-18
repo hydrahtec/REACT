ebf07b5154f684030d80f4d78def1a1aca20e43d
Eliel S <hydrahtec@gmail.com>
2024-01-18 05:48:26 -0300

    add form

--- aulas/src/App/pages/login/Login.tsx
+++ aulas/src/App/pages/login/Login.tsx
@@ -1,17 +1,41 @@
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
+
+// export const Login = () => {
+//     let navigate = useNavigate();
+
+//     const handleClick = () => {
+//         navigate("/pagina-inicial") ;
+//     }
+
+
+//     return (
+//         <div>
+//         <p>Entrar: </p>
+//         <button onClick={handleClick}>Página principal</button>
+//         </div>
+//     );
+// };
 
 export const Login = () => {
-    let navigate = useNavigate();
-
-    const handleClick = () => {
-        navigate("/pagina-inicial") ;
-    }
-
-
+   
+   
     return (
         <div>
-        <p>Entrar: </p>
-        <button onClick={handleClick}>Página principal</button>
+            <form>
+                <label>
+                    <span>Email: </span>
+                    <input />
+                </label>
+
+                <label>
+                    <span>Senha: </span>
+                    <input type="password" />
+                </label>
+
+                <button type="button" >
+                    Entrar
+                </button>
+            </form>
         </div>
     );
 };
