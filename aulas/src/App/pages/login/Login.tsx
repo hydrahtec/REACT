85952248bf16bf6089c4894ca184ec6253854cb7
Eliel S <hydrahtec@gmail.com>
2024-01-18 20:12:46 -0300

    useEffect update

--- aulas/src/App/pages/login/Login.tsx
+++ aulas/src/App/pages/login/Login.tsx
@@ -16,17 +16,39 @@
 //     );
 // };
 
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const Login = () => {
    const [email, setEmail] = useState("");
    const [password, setPassword] = useState("");
+
+   let navigate= useNavigate();
    
+useEffect(() => {
+    if (window.confirm('Você tem certeza que quer entrar?')) {
+        console.log('Entrar');
+    } else {
+        console.log('Sair');
+    };
+}, []);
+
+useEffect(() => {
+    console.log(email);
+}, [email]);
+
+useEffect(() => {
+    console.log(password);
+}, [password]);
+
  const cliqueBotao = () => {
     console.log(email);
     console.log(password);
  };
 
+const voltar = () => {
+    navigate('/pagina-inicial');
+}
 return (
     <div>
         <form>
@@ -43,6 +65,7 @@ return (
             <button type="button" onClick={cliqueBotao} >
                 Entrar
             </button>
+        <button type='button' onClick={voltar}>Voltar</button>
         </form>
     </div>
     );
