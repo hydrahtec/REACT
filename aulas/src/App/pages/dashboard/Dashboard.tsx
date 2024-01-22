c59a229e3516e8820b183021ea45535d6d16bac9
Eliel S <hydrahtec@gmail.com>
2024-01-22 20:11:22 -0300

    useref exemplo 2

--- aulas/src/App/pages/dashboard/Dashboard.tsx
+++ aulas/src/App/pages/dashboard/Dashboard.tsx
@@ -1,10 +1,16 @@
 import {Link} from 'react-router-dom';
+import { useRef } from 'react';
 
 export const Dashboard = () => {
+    const countRef = useRef({count : 0});
+
 
     return (
         <div>
             <p>Dashboard</p>
+
+            <p>Contador: {countRef.current.count}</p>
+            <button onClick={() => countRef.current.count++}>Somar</button>
             <Link to="/entrar">Login</ Link>
         </div>
     );
