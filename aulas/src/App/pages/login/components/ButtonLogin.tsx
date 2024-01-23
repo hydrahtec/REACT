3af006165ae2c3df12cf7f328699bd052662d669
Eliel S <hydrahtec@gmail.com>
2024-01-23 16:14:12 -0300

    Update ButtonLogin.tsx
    componentisando o button

--- aulas/src/App/pages/login/components/ButtonLogin.tsx
+++ aulas/src/App/pages/login/components/ButtonLogin.tsx
@@ -5,6 +5,10 @@ interface IButtonLoginProps {
 	children: React.ReactNode;
 };
 
-export const ButtonLogin : React.FC<IButtonLoginProps> = ({}) => {
-
+export const ButtonLogin : React.FC<IButtonLoginProps> = ({type, onClick, children}) => {
+	return (
+		<button type={type} onClick={onClick} > 
+			{children}
+		</button>
+)
 };
