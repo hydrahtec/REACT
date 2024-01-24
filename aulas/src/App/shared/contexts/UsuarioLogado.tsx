f65c1405707fdf4343671cc630cf003c782c2beb
Eliel S <hydrahtec@gmail.com>
2024-01-24 10:49:42 -0300

    Usuario logado
    add component

---
+++ aulas/src/App/shared/contexts/UsuarioLogado.tsx
@@ -0,0 +1,33 @@
+import {createContext, useCallback, useEffect, useState} from 'react';
+
+interface IUsuarioLogadoContextData {
+	nomeDoUsuario: string;
+	logout: () => void; // função sem retorno
+};
+
+export const UsuarioLogadoContext = createContext<IUsuarioLogadoContextData>({} as IUsuarioLogadoContextData);
+
+interface IUsuarioLogadoProviderProps {
+	children: React.ReactNode
+};
+
+export const UsuarioLogadoProvider: React.FC<IUsuarioLogadoProviderProps> = ({children}) => {
+	const [nome, setNome] = useState('');
+	
+	useEffect(() = > {
+		setTimeout(() => {
+			setNome('Lucas');
+		}, 1000);
+	});
+
+const handleLogout = useCallback(() => {
+	console.log('Logout execultou');
+},[]);
+
+return (
+	<UsuarioLogadoContext.Provider value={{nomeDoUsuario: nome, logout: handleLogout}}>
+	{children}
+	<UsuarioLogadoContext.Provider/>
+);
+
+};
