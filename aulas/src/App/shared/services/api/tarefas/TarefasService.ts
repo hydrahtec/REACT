fe371ef2f79e15918a89c23c3a2d21dac6522236
Eliel S <135076414+hydrahtec@users.noreply.github.com>
2024-01-25 12:05:22 -0300

    tarefas
    criação e update de tarefas

--- aulas/src/App/shared/services/api/tarefas/TarefasService.ts
+++ aulas/src/App/shared/services/api/tarefas/TarefasService.ts
@@ -1,1 +1,57 @@
-export {};
+import { ApiException } from "../ApiException";
+import { Api } from "../ApiConfig";
+
+export interface ITerefa {
+    id: number;
+    title: string;
+    isCompleted: boolean;
+};
+
+const getAll= async (): Promise<ITerefa[] | ApiException> => {
+    try {
+      const {data} = await Api().get('/tarefas');
+      return data;  
+    } catch (error: any) {
+        return new ApiException(error.message || 'Erro ao consultar o registro.')
+    }
+};
+const getById = async (id: number): Promise<ITerefa | ApiException> => {
+    try {
+        const {data} = await Api().get('/tarefas/${id}');
+        return data;
+    } catch (error: any) {
+        return new ApiException(error.message || 'Erro ao consultar registro');
+    }
+};
+const create = async (dataToCreate: Omit<ITerefa, 'id'>): Promise<ITerefa | ApiException> => {
+    try {
+        const {data} = await Api().post<any>('/tarefas', dataToCreate);
+        return data;
+    } catch (error: any) {
+        return new ApiException(error.message || 'Erro ao criar o registro');
+    }
+};
+const updateById = async (id: number, dataToUpdate: ITerefa): Promise<ITerefa | ApiException> => {
+    try {
+        const {data} = await Api().put('/tarefas/${id}', dataToUpdate);
+        return data;
+    } catch (error: any) {
+        return new ApiException(error.message || 'Erro ao atualizar o registro');
+    }
+};
+const deleteById = async (id: number): Promise<undefined | ApiException> => {
+    try {
+        await Api().delete('/tarefas/${id}');
+        return undefined;
+    } catch (error: any) {
+        return new ApiException(error.message || 'Erro ao apagar o registro');
+    }
+};
+
+export const TarefasService = {
+    getAll,
+    create,
+    getById,
+    updateById,
+    deleteById,
+};
